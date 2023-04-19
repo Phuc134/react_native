90943abb3d365aa0f64f10f10800ae2d30d31391
unknown <nkt14042001@gmail.com>
2023-04-19 14:37:18 +0700

    .

---
+++ components/ContactsMenu.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+const ContactsMenuButtons = [
+    {
+        type: "starred",
+        name: "starred"
+    },
+    {
+        type: "contact",
+        name: "Jessy The",
+        photo: require("")
+    },
+    {
+        type: "contact",
+        name: "Nazarizy She",
+        photo: require("")
+    },
+    {
+        type: "contact",
+        name: "Rafelf",
+        photo: require("")
+    },
+]
+function ContactsMenu() {
+    return (
+        <View style={styles.container}>
+            {/*Contact Container */}
+            {ContactsMenuButtons.map((contact, index) =>
+                <View
+                    key={index}
+                    style={styles.row}>
+
+                    {/*image */}
+                    {contact.type == "starred" ? (
+                        <View style={styles.starredIcon}>
+                            <AntDesgin name="star" size={30} color="#efefef" />
+                        </View>) :
+                        (
+                            <Image source={contact.photo} style={styles.image} />
+                        )
+                    }
+                    {/*Text */}
+                    <Text style={styles.text}>
+                        {contact.name}
+                    </Text>
+                </View>
+            )}
+        </View>
+    )
+}
+export default ContactsMenu
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        marginTop: 20,
+        alignItems: "center"
+    },
+    starredIcon: {
+        backgroundColor: "#333333",
+        width: 55,
+        height: 55,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20
+    },
+    text: {
+        color: "white",
+        paddingLeft: 15,
+        fontSize: 18
+    },
+    image: {
+        width: 55,
+        height: 55,
+        borderRadius: 20
+    }
+
+})
